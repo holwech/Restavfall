c1a73355522a51ef4d856435218b0d81d0cc2f0f
Håkon Bråten <hakbra@gmail.com>
2015-03-08 10:11:04 +0100

    Lagt til post-request for root

--- restavfall/config/routes.rb
+++ restavfall/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'users#index', via: :get
+  root to: 'users#index', via: [:get, :post]
   get 'auth/facebook', as: "auth_provider"
   get 'auth/facebook/callback', to: 'users#login'
 end
