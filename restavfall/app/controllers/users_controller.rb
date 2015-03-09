3661e1297c59a8742701b5aaf3eae8edee8ffc5c
Olav Ljosland <olavlj@me.com>
2015-03-09 16:24:01 +0100

    Skriver ut hello world

--- restavfall/app/controllers/users_controller.rb
+++ restavfall/app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
-
   def index
+  	@oauth = Koala::Facebook::OAuth.new(FACEBOOK_CONFIG["app_id"], FACEBOOK_CONFIG["secret"], "https://#{request.host}:#{request.port}/auth/facebook/callback")
+  	session['oauth'] = @oauth
 
+  	redirect_to @oauth.url_for_oauth_code
   end
 
   def login
-    @user = User.koala(request.env['omniauth.auth']['credentials'])
+  	#session['access_token'] = session['oauth'].get_access_token(params[:code])
+    #@user = User.koala(session['access_token'])
+    @data = session['oauth'].class.name
   end
+
+
 
 end
