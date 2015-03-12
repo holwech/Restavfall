94e0afc1eb3f07dbe61c7877c012c3f50ae34204
Joachim Holwech <joachim.holwech@gmail.com>
2015-03-12 11:30:41 +0100

    La til et nytt testview

--- restavfall/app/controllers/users_controller.rb
+++ restavfall/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+
   def index
-  	@oauth = Koala::Facebook::OAuth.new(FACEBOOK_CONFIG["app_id"], FACEBOOK_CONFIG["secret"], "https://#{request.host}:#{request.port}/auth/facebook/callback")
+  	@oauth =   Koala::Facebook::OAuth.new(FACEBOOK_CONFIG["app_id"], FACEBOOK_CONFIG["secret"], "https://#{request.host}:#{request.port}/auth/facebook/callback")
   	session['oauth'] = @oauth
 
   	redirect_to @oauth.url_for_oauth_code
@@ -12,6 +13,12 @@ class UsersController < ApplicationController
     @data = session['oauth'].class.name
   end
 
+  def testNext
+    @facebook_cookies ||= Koala::Facebook::OAuth.new('649498578495089', '845a0ce1a5a8be107cfc5b29bf94f634').get_user_info_from_cookie(cookies)
+    #@access_token = @facebook_cookies["access_token"]
+    #@graph = Koala::Facebook::API.new(@access_token)
+    #@me = @graph.get_object("me")
+  end
 
 
 end
--- restavfall/config/routes.rb
+++ restavfall/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'users#index', via: [:get, :post]
   get 'auth/facebook', as: "auth_provider"
   get 'auth/facebook/callback', to: 'users#login'
+  get '/testNext', to: 'users#testNext'
 end
