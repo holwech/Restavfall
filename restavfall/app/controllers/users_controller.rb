01a181b6efdb70ba31ee1a89f80beec3350d84b0
Håkon Bråten <hakbra@gmail.com>
2015-03-16 14:27:33 +0100

    Fikset permissions

--- restavfall/app/controllers/users_controller.rb
+++ restavfall/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   	@oauth =   Koala::Facebook::OAuth.new(FACEBOOK_CONFIG["app_id"], FACEBOOK_CONFIG["secret"], "https://#{request.host}:#{request.port}/auth/facebook/callback")
   	session['oauth'] = @oauth
 
-  	redirect_to @oauth.url_for_oauth_code
+  	redirect_to @oauth.url_for_oauth_code(:permissions => ['user_friends', 'user_photos'])
   end
 
   def login
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = Koala::Facebook::API.new(token)
     @profile_image = @user.get_picture('me')
     @name = @user.get_object('me')['name']
+    @permissions = @user.get_connections('me', 'permissions')
 
     ff = FriendFinder.new(@user)
     ff.run_analysis
