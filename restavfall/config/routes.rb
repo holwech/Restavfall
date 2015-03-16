07f83ab4a965375551a67a36bec18290a02d1219
Joachim Holwech <joachim.holwech@gmail.com>
2015-03-16 18:56:42 +0100

    Laget controller for events

--- restavfall/config/routes.rb
+++ restavfall/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :events
+
   root to: 'users#index', via: [:get, :post]
   get 'auth/facebook', as: "auth_provider"
   get 'auth/facebook/callback', to: 'users#login'
