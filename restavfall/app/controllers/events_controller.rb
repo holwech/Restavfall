c2e73b314ffb3a251568ee29b209ee3925d12fad
Joachim Holwech <joachim.holwech@gmail.com>
2015-03-17 20:16:46 +0100

    Gjorde events controller veldig lik user controller for å fikse en feil med token

--- restavfall/app/controllers/events_controller.rb
+++ restavfall/app/controllers/events_controller.rb
@@ -1,6 +1,17 @@
 class EventsController < ApplicationController
 
   def index
+    @oauth =   Koala::Facebook::OAuth.new(FACEBOOK_CONFIG["app_id"], FACEBOOK_CONFIG["secret"], "https://#{request.host}:#{request.port}/events/show")
+    session['oauth'] = @oauth
+
+    redirect_to @oauth.url_for_oauth_code(:permissions => 
+              ['user_friends', 'user_photos', 'user_events',
+               'read_stream', 'publish_actions'])
+  end
+
+
+
+  def show
     auth = request.env["omniauth.auth"]
     token = auth['credentials']['token']
     @user = Koala::Facebook::API.new(token)
--- restavfall/config/routes.rb
+++ restavfall/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'auth/facebook', as: "auth_provider"
   get 'auth/facebook/callback', to: 'users#login'
   get 'events', to: 'events#index'
+  get 'events/show', to: 'events#show'
 end
