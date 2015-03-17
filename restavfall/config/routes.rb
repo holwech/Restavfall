f1ca2b8d8073a95a5c7b7730c279882030cb296b
Joachim Holwech <joachim.holwech@gmail.com>
2015-03-17 19:41:06 +0100

    Ryddet opp i events controller

--- restavfall/config/routes.rb
+++ restavfall/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'users#index', via: [:get, :post]
   get 'auth/facebook', as: "auth_provider"
   get 'auth/facebook/callback', to: 'users#login'
+  get 'events', to: 'events#index'
 end
