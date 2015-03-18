3e0cb9278419ab54f07d2cb9ac38fc26f1dc2deb
Joachim Holwech <joachim.holwech@gmail.com>
2015-03-18 17:35:08 +0100

    Ryddet opp i modelsfilene

--- restavfall/app/models/event.rb
+++ restavfall/app/models/event.rb
@@ -1,7 +1,3 @@
 class Event < ActiveRecord::Base
-  def self.koala(auth)
-    access_token = auth['token']
-    facebook = Koala::Facebook::API.new(access_token)
-    facebook.get_object("me?fields=name,picture")
-  end
+
 end
