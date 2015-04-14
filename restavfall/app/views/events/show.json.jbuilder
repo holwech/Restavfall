fe52f536472bfeef34d36d4ee4968e98d135b43a
Håkon Bråten <hakbra@gmail.com>
2015-04-14 14:25:10 +0200

    Fikset event controller og flyttet uno.erb.html

--- restavfall/app/views/events/show.json.jbuilder
+++ restavfall/app/views/events/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @event, :id, :created_at, :updated_at
+json.extract! @event, :id, :name, :url, :time, :fbpageID, :fbeventID, :created_at, :updated_at
--- restavfall/test/controllers/events_controller_test.rb
+++ restavfall/test/controllers/events_controller_test.rb
@@ -18,7 +18,7 @@ class EventsControllerTest < ActionController::TestCase
 
   test "should create event" do
     assert_difference('Event.count') do
-      post :create, event: {  }
+      post :create, event: { fbeventID: @event.fbeventID, fbpageID: @event.fbpageID, name: @event.name, time: @event.time, url: @event.url }
     end
 
     assert_redirected_to event_path(assigns(:event))
@@ -35,7 +35,7 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test "should update event" do
-    patch :update, id: @event, event: {  }
+    patch :update, id: @event, event: { fbeventID: @event.fbeventID, fbpageID: @event.fbpageID, name: @event.name, time: @event.time, url: @event.url }
     assert_redirected_to event_path(assigns(:event))
   end
 
