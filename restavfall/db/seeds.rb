820c56c47e9270e570e100489f6f884b582a3522
Olav Ljosland <olavlj@me.com>
2015-04-16 16:05:20 +0200

    La til bilde paa uno

--- restavfall/db/seeds.rb
+++ restavfall/db/seeds.rb
@@ -1,8 +1,8 @@
 Event.delete_all
-Event.create(name:'Lemaitre', time: '2015-08-12', fbeventID: '812458895503921', url: 'https://www.uka.no/program/5-norsk-indieelektronika-kommer-til-uka/', fbpageID: '110144622369960');
-Event.create(name:'Action Bronson', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124');
-Event.create(name:'lol', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124');
-Event.create(name:'tset', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124');
-Event.create(name:'Actionafs', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124');
-Event.create(name:'afs Brdsonson', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124');
-Event.create(name:'dddddd Bronson', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124');
+Event.create(name:'Lemaitre', time: '2015-08-12', fbeventID: '812458895503921', url: 'https://www.uka.no/program/5-norsk-indieelektronika-kommer-til-uka/', fbpageID: '110144622369960', img: 'lemaitre.jpg');
+Event.create(name:'Action Bronson', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124', img: 'actionbronson.jpg');
+Event.create(name:'lol', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124', img: 'actionbronson.jpg');
+Event.create(name:'tset', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124', img: 'actionbronson.jpg');
+Event.create(name:'Actionafs', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124', img: 'actionbronson.jpg');
+Event.create(name:'afs Brdsonson', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124', img: 'actionbronson.jpg');
+Event.create(name:'dddddd Bronson', time: '2015-08-12', fbeventID: '910627385655121', url: 'https://www.uka.no/program/15-action-bronson-til-uka-15/', fbpageID: '111529277124', img: 'actionbronson.jpg');
