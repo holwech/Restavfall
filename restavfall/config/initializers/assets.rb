03c251675a0d9ad872c264b216dc0e810050aa6b
Håkon Bråten <haakbraa@online.no>
2015-09-02 20:06:33 +0200

    Endret innstillinger

--- restavfall/config/initializers/assets.rb
+++ restavfall/config/initializers/assets.rb
@@ -4,7 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join('fonts');
 Rails.application.config.assets.precompile += %w( analyse.js )
 Rails.application.config.assets.precompile += %w( test.css )
 Rails.application.config.assets.precompile += %w( test.js )
