c86a64bfce1fedf4cfd3b162f07e32546a77a3bd
Håkon Bråten <hakonbraten@gmail.com>
2015-09-03 18:30:38 +0200

    Flyttet test css og js til home css og js

--- restavfall/config/initializers/assets.rb
+++ restavfall/config/initializers/assets.rb
@@ -5,8 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.precompile += %w( analyse.js )
-Rails.application.config.assets.precompile += %w( test.css )
-Rails.application.config.assets.precompile += %w( test.js )
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( home.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
